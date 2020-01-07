81825a4df95f097c03810261574af4a9db6408a7
hrvwnd <harveyawendon@gmail.com>
2020-01-07 22:49:31 +0000

    fixing extra speechmarks on ingredients list and started service 4

--- 4-final-recipe-generator/application/functions.py
+++ 4-final-recipe-generator/application/functions.py
@@ -1,10 +1,26 @@
-from random import randint
+#import requests
 
-# Random method of cooking generator 
-def method_generator():
-    list_of_methods = ["Fried", "Roasted", "Boiled", "Steamed", "Grilled", "Baked", "Stewed"]
-    random_number = randint(0,len(list_of_methods))
-    print (random_number)
-    method = list_of_methods[random_number]
-    return method
-#print (method_generator())
+
+def join_list(list_of_ingredients, method_of_cooking):
+    list_of_ingredients.append(method_of_cooking)
+    return list_of_ingredients
+
+def count_letters(list_of_ingredients_and_method):
+    pass
+    # CHANGE ME 
+    # make function to count the number of letters in a list
+
+
+
+def add_or_delete_parts(ingredients, method):
+    list_of_ingredients_and_method = join_list(ingredients, method)
+    number_of_letters = count_letters(list_of_ingredients_and_method)
+    # CHANGE ME 
+    # Add code for logic on either adding, removing or changing one ingredients based on 
+    # length of the string
+
+
+ingredients = ['Green Beans', 'Peppers', 'Potatoes', 'Avocado', 'Mushrooms']
+print (ingredients[1])
+ingredients.append('stewed')
+print (ingredients)
--- 4-final-recipe-generator/application/routes.py
+++ 4-final-recipe-generator/application/routes.py
@@ -1,7 +1,9 @@
 from application import app
-from application.functions import method_generator
+from application.functions import add_or_delete_parts
 
 @app.route("/",methods = ["GET", "POST"])
-def method_generate():
-    post = method_generator()
+def final_recipe_parts():
+    ingredients_service = "http://2-ingredient-generator:5000/"
+    method_service = "http://3-method-generator:5000/"
+    post = add_or_delete_parts(ingredients_service, method_service)
     return post
