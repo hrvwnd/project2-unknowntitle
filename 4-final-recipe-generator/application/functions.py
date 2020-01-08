59bd72c5422519f667128a8d5f3642a19abd2297
hrvwnd <harveyawendon@gmail.com>
2020-01-08 22:26:28 +0000

    service4-final-recipe-generator functions.py finished

--- 4-final-recipe-generator/application/functions.py
+++ 4-final-recipe-generator/application/functions.py
@@ -33,6 +33,7 @@ def random_remove(list_of_ingredients_and_method, remove, spare_ingredients):
         list_of_ingredients_and_method.remove(list_of_ingredients_and_method[random_number]) # removes ingredient
         list_of_ingredients_and_method.append(method) # Adds method back on to list in same position
         return list_of_ingredients_and_method
+
     elif not remove:
         different_ingredients = find_different_ingredients(list_of_ingredients_and_method, spare_ingredients) # ensures no repeat ingredients
         if not different_ingredients:
@@ -41,6 +42,7 @@ def random_remove(list_of_ingredients_and_method, remove, spare_ingredients):
         list_of_ingredients_and_method.append(different_ingredients[random_number])
         list_of_ingredients_and_method.append(method) # Adds method back on to the list
         return list_of_ingredients_and_method
+
     elif remove == "replace":
         different_ingredients = find_different_ingredients(list_of_ingredients_and_method, spare_ingredients)
         if not different_ingredients:
@@ -50,7 +52,8 @@ def random_remove(list_of_ingredients_and_method, remove, spare_ingredients):
         list_of_ingredients_and_method.append(different_ingredients[random_number])
         list_of_ingredients_and_method.append(method) # Adds method back on to the list
         return list_of_ingredients_and_method
-print (random_remove(ingredients, "replace", ingredients2))
+
+#print (random_remove(ingredients, "replace", ingredients2))
 
 
 def count_letters(list_of_ingredients_and_method):
@@ -58,23 +61,23 @@ def count_letters(list_of_ingredients_and_method):
     for item in list_of_ingredients_and_method:
         count += len(item)
     return count
+
 # print (count_letters(ingredients))
+
 
 # A function that factors in how long the list of ingredients is based on the length of the combined letters in all ingredients
 def add_or_delete_parts(ingredients, method, spare_ingredients):
     list_of_ingredients_and_method = join_list(ingredients, method)
     count = count_letters(list_of_ingredients_and_method)
-    if count >=40:
+    if count >=40: # If over 40 characters, remvoe an ingredient at random
         return random_remove(list_of_ingredients_and_method, True, spare_ingredients)
-    elif 30 <= count < 40:
+
+    elif 30 <= count < 40: # Between 30-40 keep ingredients list the same
         return list_of_ingredients_and_method
-    elif 20 < count < 30:
+
+    elif 20 < count < 30: # Between 20-30, replace an ingredient in the list
         return random_remove(list_of_ingredients_and_method, "replace", spare_ingredients)
-    elif count <= 20:
+
+    elif count <= 20: # lower than 20, add an ingredient to the list
         return random_remove(list_of_ingredients_and_method, False, spare_ingredients)
         
-
-
-    # CHANGE ME 
-    # Add code for logic on either adding, removing or changing one ingredients based on 
-    # length of the string
