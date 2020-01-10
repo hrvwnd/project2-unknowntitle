54804e5d8aa4264b1326fb96dd937e27ffe67277
hrvwnd <harveyawendon@gmail.com>
2020-01-10 21:37:07 +0000

    bug fixing json passing

--- 4-final-recipe-generator/application/routes.py
+++ 4-final-recipe-generator/application/routes.py
@@ -7,13 +7,15 @@ def final_recipe_parts():
     ingredients_service = requests.get("http://2-ingredient-generator:5002/")
     spare_ingredients_service = requests.get("http://2-ingredient-generator:5002/")
     method_service = requests.get("http://3-method-generator:5003/")
-    ingredients_service = ingredients_service.text.split(",")
-    print ("TEST PRINT ")
-    print (ingredients_service)
-    print (spare_ingredients_service.text)
-    spare_ingredients_service = spare_ingredients_service.text.split(",")
+    ingredients_service = ingredients_service.text
+    spare_ingredients_service = spare_ingredients_service.text
+    method_service = method_service.text    
+    #ingredients_service = ingredients_service.text.split(",")
+    #print ("TEST PRINT ")
     print (spare_ingredients_service)
-    method_service = method_service.text.split(",")
+    #spare_ingredients_service = spare_ingredients_service.text.split(",")
+    print (ingredients_service)
+    #method_service = method_service.text.split(",")
     post = add_or_delete_parts(ingredients_service, method_service, spare_ingredients_service)
     #post = np.asarray(post) # converts to array for json transfer
     post = dumps(post)
