004df7586314d1de2ac5977b12ca63a56d044e8f
hrvwnd <harveyawendon@gmail.com>
2020-01-10 18:05:10 +0000

    making lists json compatable because json is fussy

--- 4-final-recipe-generator/application/routes.py
+++ 4-final-recipe-generator/application/routes.py
@@ -8,6 +8,10 @@ def final_recipe_parts():
     ingredients_service = requests.get("http://2-ingredient-generator:5002/")
     spare_ingredients_service = requests.get("http://2-ingredient-generator:5002/")
     method_service = requests.get("http://3-method-generator:5003/")
+    print ("LOOK AT MEEEEE abcdefg")
+    print (ingredients_service)
+    print (spare_ingredients_service)
+    print(method_service)
     post = add_or_delete_parts(ingredients_service, method_service, spare_ingredients_service)
     #post = np.asarray(post) # converts to array for json transfer
     json.dumps(post)
