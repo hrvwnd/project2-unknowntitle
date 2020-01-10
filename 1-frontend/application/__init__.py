95f1631d1961fae00fe573876b6c5085754bc776
hrvwnd <harveyawendon@gmail.com>
2020-01-10 14:23:18 +0000

    changing ports on app.py

--- 1-frontend/application/__init__.py
+++ 1-frontend/application/__init__.py
@@ -5,8 +5,8 @@ from os import getenv
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']=('mysql+pymysql://' + getenv('MYSQL_USER') + ':' + getenv('MYSQL_PASSWORD') + '@' + getenv('MYSQL_HOST') + '/' + getenv('MYSQL_DB'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = getenv('SECRET_KEY')
-#app.config['SECRET_KEY'] = '43riu34jhferugfhrf83h98eh3fuwhergiurhe'
+#app.config['SECRET_KEY'] = getenv('SECRET_KEY')
+app.config['SECRET_KEY'] = '43riu34jhferugfhrf83h98eh3fuwhergiurhe'
 
 db = SQLAlchemy(app)
 
