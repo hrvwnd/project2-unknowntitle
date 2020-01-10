a602b6bfa3decb60652fe1d00e28423734eb26a3
hrvwnd <harveyawendon@gmail.com>
2020-01-10 17:41:00 +0000

    making lists json compatable because json is fussy

--- 2-ingredient-generator/application/routes.py
+++ 2-ingredient-generator/application/routes.py
@@ -2,11 +2,13 @@ from application import app
 from application.functions import random_list_item, random_number_generator, \
     vegetarian, list_of_ingredients
 import numpy as np
+import json
 
 @app.route('/', methods=["GET", "POST"])
 def generate_recipe_ingredients():
     post = list_of_ingredients()
-    post = np.asarray(post)
+    #post = np.asarray(post)
+    json.dumps(post)
     return post
 
 #print (generate_recipe_ingredients())
--- 4-final-recipe-generator/application/routes.py
+++ 4-final-recipe-generator/application/routes.py
@@ -2,12 +2,13 @@ import requests
 from application import app
 from application.functions import add_or_delete_parts
 import numpy as np
-
+import json
 @app.route("/",methods = ["GET", "POST"])
 def final_recipe_parts():
     ingredients_service = requests.get("http://2-ingredient-generator:5002/")
     spare_ingredients_service = requests.get("http://2-ingredient-generator:5002/")
     method_service = requests.get("http://3-method-generator:5003/")
     post = add_or_delete_parts(ingredients_service, method_service, spare_ingredients_service)
-    post = np.asarray(post) # converts to array for json transfer
+    #post = np.asarray(post) # converts to array for json transfer
+    json.dumps(post)
     return post
