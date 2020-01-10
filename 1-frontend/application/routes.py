d16ec58cc19b4ff25b97a68e993d500d3d70a9ac
hrvwnd <harveyawendon@gmail.com>
2020-01-10 22:28:07 +0000

    bug fixing json passing

--- 1-frontend/application/routes.py
+++ 1-frontend/application/routes.py
@@ -12,6 +12,12 @@ import requests
 def home():
     GIform = GenerateIngredientsForm()
     recipe_name_form = RecipeNameForm()
+    #HERE
+    recipe_response = requests.get("http://4-final-recipe-generator:5004/")
+    recipe_response = recipe_response.text
+    print (recipe_response)
+    recipe_response = eval(str(recipe_response))
+    #HERE
     if GIform.is_submitted():
         recipe_response = requests.get("http://4-final-recipe-generator:5004/")
         recipe_response = recipe_response.text
@@ -36,7 +42,9 @@ def home():
         flash ("Recipe saved" + str(recipe_name_form.recipe_name.data))
         return render_template(url_for('recipes'))
 
-    return render_template("home.html", title = "home", GIform = GIform, recipe_name_form= recipe_name_form)
+    return render_template("home.html", title = "home", GIform = GIform, recipe_name_form = recipe_name_form, \
+                list_of_ingredients_and_method = list_of_ingredients_and_method
+    #return render_template("home.html", title = "home", GIform = GIform, recipe_name_form= recipe_name_form)
 
 
 @app.route('/recipes',methods=["GET","POST"])
