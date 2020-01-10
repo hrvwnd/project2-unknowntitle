abf690a87fe09392780bed4857abb02538571a7a
hrvwnd <harveyawendon@gmail.com>
2020-01-10 15:03:58 +0000

    fixed service3 bug and service1 home.html typo

--- 3-method-generator/application/functions.py
+++ 3-method-generator/application/functions.py
@@ -3,8 +3,9 @@ from random import randint
 # Random method of cooking generator 
 def method_generator():
     list_of_methods = ["Fried", "Roasted", "Boiled", "Steamed", "Grilled", "Baked", "Stewed"]
-    random_number = randint(0,len(list_of_methods))
+    random_number = randint(0,len(list_of_methods)-1)
+    #print (random_number)
     #print (random_number)
     method = list_of_methods[random_number]
     return method
-print (method_generator())
+#print (method_generator())
