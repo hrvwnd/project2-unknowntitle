8254154df071d2464de2fcbffa0ddaae204e43a2
hrvwnd <harveyawendon@gmail.com>
2020-01-10 21:21:13 +0000

    bug fixing json passing

--- 2-ingredient-generator/application/routes.py
+++ 2-ingredient-generator/application/routes.py
@@ -1,14 +1,12 @@
 from application import app 
 from application.functions import random_list_item, random_number_generator, \
     vegetarian, list_of_ingredients
-import numpy as np
 from json import dumps
 
 @app.route('/', methods=["GET", "POST"])
 def generate_recipe_ingredients():
     post = list_of_ingredients()
-    #post = np.asarray(post)
-    #post = json.dumps(post)
+    post = dumps(post)
     #post = post.text
     return post
 
--- 4-final-recipe-generator/application/routes.py
+++ 4-final-recipe-generator/application/routes.py
@@ -1,21 +1,21 @@
 import requests
 from application import app
 from application.functions import add_or_delete_parts
-import numpy as np
-import json
+from json import dumps
 @app.route("/",methods = ["GET", "POST"])
 def final_recipe_parts():
     ingredients_service = requests.get("http://2-ingredient-generator:5002/")
     spare_ingredients_service = requests.get("http://2-ingredient-generator:5002/")
     method_service = requests.get("http://3-method-generator:5003/")
-    #ingredients_service = ingredients_service.split(",")
-    #spare_ingredients_service = spare_ingredients_service.split(",")
-    #method_service = method_service.split(",")
+    ingredients_service = ingredients_service.split(",")
+    print ("TEST PRINT ")
+    print (ingredients_service)
+    spare_ingredients_service = spare_ingredients_service.split(",")
+    method_service = method_service.split(",")
     post = add_or_delete_parts(ingredients_service, method_service, spare_ingredients_service)
     #post = np.asarray(post) # converts to array for json transfer
-    #json.dumps(post)
-    #post = post.text
+    post = dumps(post)
     return post
 
 print (final_recipe_parts())
-print (json.dumps(final_recipe_parts))
+print (dumps(final_recipe_parts))
