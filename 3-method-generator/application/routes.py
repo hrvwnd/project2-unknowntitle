8628ae7e35b7819ccaa2bcb9dfcd84f3f6ffe925
hrvwnd <harveyawendon@gmail.com>
2020-01-10 17:36:29 +0000

    making lists into arrays because json is fussy

--- 3-method-generator/application/routes.py
+++ 3-method-generator/application/routes.py
@@ -1,8 +1,7 @@
-#from application import app
-#from application.functions import method_generator
-from functions import method_generator
-#@app.route("/",methods = ["GET", "POST"])
+from application import app
+from application.functions import method_generator
 
+@app.route("/",methods = ["GET", "POST"])
 def method_generate():
     post = method_generator()
     return post
