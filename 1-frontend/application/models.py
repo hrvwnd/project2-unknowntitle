a5fa63d875cc7254ef067abdff152f6ed6341523
hrvwnd <harveyawendon@gmail.com>
2020-01-11 14:59:25 +0000

    home.html adjustments

--- 1-frontend/application/models.py
+++ 1-frontend/application/models.py
@@ -4,7 +4,7 @@ class Recipes(db.Model):
         id = db.Column(db.Integer, primary_key=True, unique = True)
         name = db.Column(db.String(30), nullable=False, unique= True)
         item1 = db.Column(db.String(15), nullable=True, unique= False)
-        item2 = db.Column(db.String(15), nullable=True, unique= False
+        item2 = db.Column(db.String(15), nullable=True, unique= False)
         item3 = db.Column(db.String(15), nullable=True, unique= False)
         item4 = db.Column(db.String(15), nullable=True, unique= False)
         item5 = db.Column(db.String(15), nullable=True, unique= False)
