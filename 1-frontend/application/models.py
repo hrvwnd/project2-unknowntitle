a616da8eaba4d7f0e08ffd2ae7eabeb517855cf9
hrvwnd <harveyawendon@gmail.com>
2020-01-11 19:20:24 +0000

    Adding database (re)creation tools

--- 1-frontend/application/models.py
+++ 1-frontend/application/models.py
@@ -3,13 +3,13 @@ from application import db
 class Recipes(db.Model):
         id = db.Column(db.Integer, primary_key=True, unique = True)
         name = db.Column(db.String(30), nullable=False, unique= True)
-        item1 = db.Column(db.String(15), nullable=True, unique= False)
-        item2 = db.Column(db.String(15), nullable=True, unique= False)
-        item3 = db.Column(db.String(15), nullable=True, unique= False)
-        item4 = db.Column(db.String(15), nullable=True, unique= False)
-        item5 = db.Column(db.String(15), nullable=True, unique= False)
-        item6 = db.Column(db.String(15), nullable=True, unique= False)
-        item7 = db.Column(db.String(15), nullable=True, unique= False)
+        item1 = db.Column(db.String(30), nullable=True, unique= False)
+        item2 = db.Column(db.String(30), nullable=True, unique= False)
+        item3 = db.Column(db.String(30), nullable=True, unique= False)
+        item4 = db.Column(db.String(30), nullable=True, unique= False)
+        item5 = db.Column(db.String(30), nullable=True, unique= False)
+        item6 = db.Column(db.String(30), nullable=True, unique= False)
+        item7 = db.Column(db.String(30), nullable=True, unique= False)
 
 class Ingredients(db.Model):
 
