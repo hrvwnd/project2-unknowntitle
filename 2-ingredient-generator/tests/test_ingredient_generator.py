f5793453be83585322445da1f2b68be30d50f5b5
hrvwnd <harveyawendon@gmail.com>
2020-01-12 19:54:36 +0000

    removed db from test

--- 2-ingredient-generator/tests/test_ingredient_generator.py
+++ 2-ingredient-generator/tests/test_ingredient_generator.py
@@ -31,7 +31,7 @@ class TestServiceFunction(TestBase):
         self.assertTrue(3 <= random_number <7)
 
     def test_vegetarian():
-        self.routes.vegetarian() == True or routes.vegetarian() == False
+        assert routes.vegetarian() == True or routes.vegetarian() == False
     
 
 
