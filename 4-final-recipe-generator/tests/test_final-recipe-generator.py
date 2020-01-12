60760ddb552740a37267e96173dd1c9e6e859109
hrvwnd <harveyawendon@gmail.com>
2020-01-12 20:44:03 +0000

    typo in test name

--- 4-final-recipe-generator/tests/test_final-recipe-generator.py
+++ 4-final-recipe-generator/tests/test_final-recipe-generator.py
@@ -25,7 +25,7 @@ class TestServiceRoutes(TestBase):
 '''    
 
 class TestServiceFunctions(TestBase):
-            ingredients = ["Beef", "Tomatoes", "Onions", "Cabbage"]
+        ingredients = ["Beef", "Tomatoes", "Onions", "Cabbage"]
         method = "Grilled"
         spare_ingredients = ["Beef","Onions","Olives","Pork","Red Cabbage","Brussels"]
     def test_add_or_delete_parts(self,ingredients,method,spare_ingredients):
