b490dffe2bdf4469f7b096341af51454531e375d
hrvwnd <harveyawendon@gmail.com>
2020-01-12 11:18:32 +0000

    testing removing generate button

--- 1-frontend/application/routes.py
+++ 1-frontend/application/routes.py
@@ -22,6 +22,7 @@ def home():
             recipe_response = recipe_response.text
             recipe_response = eval(str(recipe_response))
             list_of_ingredients_and_method = recipe_response
+            print (list_of_ingredients_and_method)
 
             if recipe_name_form.validate_on_submit():
                 print ("-----------------------------------------READ ME --------------------------")
@@ -59,39 +60,10 @@ def home():
 
         else:
             return "404- ingredients not found"
-    '''
-    if recipe_name_form.validate_on_submit():
-        print ("-----------------------------------------READ ME --------------------------")
-        print (list_of_ingredients_and_method)
 
-        if list_of_ingredients_and_method[0] == "Press Generate button to create a potential recipe" or \
-             list_of_ingredients_and_method[0] == "You need to generate recipes first": 
+        return render_template("home.html", title = "home", GIform=GIform, recipe_name_form = recipe_name_form, \
+            list_of_ingredients_and_method = list_of_ingredients_and_method) 
 
-            return render_template("home.html", title = "home", GIform = GIform, recipe_name_form = recipe_name_form, \
-                list_of_ingredients_and_method = ["You need to generate recipes first"])
-
-        while len(list_of_ingredients_and_method) < 7: # adds empty elements to the list if not at max length
-            list_of_ingredients_and_method.append(" ")
-
-        print ("------------------------------READ ME ---------------------------------")
-        print ("making sure theres 7 items in list")
-        print (len(list_of_ingredients_and_method))
-        print (list_of_ingredients_and_method)
-        new_recipe = Recipes(
-            name = recipe_name_form.recipe_name.data,
-            item1 = list_of_ingredients_and_method[0],
-            item2 = list_of_ingredients_and_method[1],
-            item3 = list_of_ingredients_and_method[2],
-            item4 = list_of_ingredients_and_method[3],
-            item5 = list_of_ingredients_and_method[4],
-            item6 = list_of_ingredients_and_method[5],
-            item7 = list_of_ingredients_and_method[6]
-        )
-        db.session.add(new_recipe)
-        db.session.commit()
-        flash ("Recipe saved" + str(recipe_name_form.recipe_name.data))
-        return redirect(url_for('recipes'))
-    '''
     return render_template("home.html", title = "home", GIform = GIform, recipe_name_form = recipe_name_form, \
         list_of_ingredients_and_method = list_of_ingredients_and_method)
 
