d1accb22428b3341bab5a65018e9897aa1049abb
hrvwnd <harveyawendon@gmail.com>
2020-01-22 16:26:32 +0000

    added xray to __init__.py

--- 1-frontend/application/__init__.py
+++ 1-frontend/application/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']=('mysql+pymysql://' + getenv('MYSQL_USER') + ':' + getenv('MYSQL_PASSWORD') + '@' + getenv('MYSQL_HOST') + '/' + getenv('MYSQL_DB'))
@@ -10,5 +12,7 @@ app.config['SECRET_KEY'] = '43riu34jhferugfhrf83h98eh3fuwhergiurhe'
 
 db = SQLAlchemy(app)
 
+xray_recorder.configure(service='harry', dynamic_naming='*18.130.174.90*')
+XRayMiddleware(app, xray_recorder)
 
 from application import routes
