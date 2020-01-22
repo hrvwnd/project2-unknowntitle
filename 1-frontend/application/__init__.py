69d000912a3303d624d639b1145374372f354bb7
hrvwnd <harveyawendon@gmail.com>
2020-01-22 16:32:11 +0000

    added aws-xray

--- 1-frontend/application/__init__.py
+++ 1-frontend/application/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+import requests
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']=('mysql+pymysql://' + getenv('MYSQL_USER') + ':' + getenv('MYSQL_PASSWORD') + '@' + getenv('MYSQL_HOST') + '/' + getenv('MYSQL_DB'))
@@ -12,7 +13,7 @@ app.config['SECRET_KEY'] = '43riu34jhferugfhrf83h98eh3fuwhergiurhe'
 
 db = SQLAlchemy(app)
 
-xray_recorder.configure(service='harry', dynamic_naming='*18.130.174.90*')
+xray_recorder.configure(service='1-frontend', dynamic_naming='*18.130.174.90*')
 XRayMiddleware(app, xray_recorder)
 
 from application import routes
--- 3-method-generator/application/__init__.py
+++ 3-method-generator/application/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
 from os import getenv
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+import requests
 
 app = Flask(__name__)
+
+xray_recorder.configure(service='3-method-generator', dynamic_naming='*18.130.174.90*')
+XRayMiddleware(app, xray_recorder)
 
 from application import routes
